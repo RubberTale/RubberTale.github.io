2c564bfe55726797a68522ae29df4a1e856b8eb8
RubberTale <RubberTale@users.noreply.github.com>
2026-08-16 23:41:38 +0800

    新增橡胶期货图表分析文章 & 更新deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,15 +1,15 @@
 @echo off
 
-REM ** ��������������⣬������̨����ҳ����Ϊ UTF-8 **
+REM ** ⣬̨ҳΪ UTF-8 **
 chcp 65001 > nul 
 
-REM ** �л�����ȷ����ĿĿ¼ **
-cd /d "D:\RubberTale\RubberTale.github.io\"
+REM ** 切换到正确的项目目录 **
+cd /d "D:\RubberTale\rubbertale.github.io\"
 
-echo ��������ɵ������ļ��ͻ���...
+echo ɵļͻ...
 call hexo clean
 
-echo ��ʼ���� Hexo ��̬�ļ�...
+echo ʼ Hexo ̬ļ...
 call hexo g
 
 echo ��̬�ļ�������ϣ���ʼ����...
